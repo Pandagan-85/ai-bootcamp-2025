98df1a7660b0a36823c09463c158656d2b9ce961
Veronica Schembri <veronicaschembri@gmail.com>
2025-01-26 18:19:45 +0100

    Esercizio giorno 2 - Funzioni (testing delle list comprehension e timeit)

--- python/giorno2_3/esercizi/funzioni/use_list_comp.py
+++ python/giorno2_3/esercizi/funzioni/use_list_comp.py
@@ -1,3 +1,4 @@
+
 # Studio delle list comprehension
 """
 Una list comprehension** in Python
@@ -5,6 +6,8 @@ Una list comprehension** in Python
 creare una lista a
 partire da un’altra sequenza
 """
+
+import timeit
 def pow_list_2(seq:list) -> list:
     """
         given a list, return a list with each value
@@ -30,3 +33,28 @@ def count_vowels_2(quote):
     return sum(1 for char in quote if char in vowels)
 
 print("count_vowels_2: ", count_vowels_2("abc def ghi lmn opq rst uvz"))
+
+## Test timeit
+def sum_even_numbers(seq) :
+    """
+    return the sum of all even number is the list passed
+    """
+    even = []
+    for n in seq:
+        if n % 2 == 0:
+            even.append(n)
+
+    return sum(even)
+
+## Test di velocità
+
+# Eseguire il test di tempo per le due funzioni
+seq = list(range(100))  # Definisco la sequenza da passare alle funzioni
+
+# Misura il tempo di esecuzione della funzione 1
+time_func1 = timeit.timeit('sum_even_numbers(seq)', globals=globals(), number=1000)
+print(f"Tempo per func1: {time_func1} secondi")
+
+# Misura il tempo di esecuzione della funzione 2
+time_func2 = timeit.timeit('sum_even_numbers_2(seq)', globals=globals(), number=1000)
+print(f"Tempo per func2: {time_func2} secondi")

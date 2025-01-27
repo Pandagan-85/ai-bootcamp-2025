a9df874f4c1159a07ea168f87c4eeaead0af8673
Veronica Schembri <veronicaschembri@gmail.com>
2025-01-27 13:39:39 +0100

    Esercizio Classi Conto corrente

---
+++ python/giorno2_3/esercizi/classi/esercizioContoCorrente.py
@@ -0,0 +1,24 @@
+class Account:
+    def __init__(self, name):
+        self.name = name
+        self.balance = 0
+
+    def deposit(self, num):
+        self.balance += num  # Aggiungo dindini al conto
+    def withdraw(self, num): # Prendo dindini dal conto
+        self.balance -=num
+        if self.balance <= 0:
+            print(f"InsufficientFundsError")
+
+
+account = Account(name="Rigel")
+assert account.name == "Rigel"
+assert account.balance == 0
+
+account.deposit(500)
+assert account.balance == 500
+
+account.withdraw(200)
+assert account.balance == 300
+
+account.withdraw(400)

2122c2ac3fe1a4e4d7b01e6435200b2939143d66
Veronica Schembri <veronicaschembri@gmail.com>
2025-01-30 17:45:13 +0100

    Approfondimento Linked List e update funzioni di controllo sui parametri di query e find

--- python/giorno4/main.py
+++ python/giorno4/main.py
@@ -1,4 +1,8 @@
 # Implement qui il codice
+class SearchStringTooShortError(Exception):
+    """Eccezione personalizzata per una stringa di ricerca troppo corta."""
+    pass
+
 class Directory:
     def __init__(self):
         self.users = []
@@ -10,16 +14,29 @@ class Directory:
         return self.users
     # Lazy object usando un generatore e non una lista per query a find
     def query(self, name):
+        # Di default suppongo che l'utente non sia trovato
+        found = False
         for user in self.users:
             if user.name == name:
+                # Quando trovo l'utente imposto a true
+                found = True
                 # print(user.name, user.phone)
                 yield user
+        if not found:
+                print(f"No users found with name: {name}")
     def find(self, q_string):
+        if len(q_string) < 3:
+            # Solleva un'eccezione se la stringa di ricerca è troppo corta
+            raise SearchStringTooShortError("La stringa di ricerca deve essere di almeno 2 caratteri.")
+        found = False
         for user in self.users:
             # Cerco la stringa in tutti i valori contenuti nell'oggetto user
             if any(q_string in str(value) for value in vars(user).values()):
+                found = True
                 #print(user)
                 yield user
+        if not found:
+            print(f"No users found with string: {q_string}")
 
 
 class Person:
@@ -27,7 +44,7 @@ class Person:
         self.name = name
         self.surname = surname
         self.phone = phone
-    # rappresentare obj user in modo leggibile
+    # rappresentare obj user in modo leggibile, traduco output <main.Person object at 0x100a7b4d0> in dati leggibili
     def __repr__(self):
         return f"Person(name='{self.name}', surname='{self.surname}', phone='{self.phone}')"
 

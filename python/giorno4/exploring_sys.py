8f3735a6bbee08f9ece5f97bf77711babf22fab4
Veronica Schembri <veronicaschembri@gmail.com>
2025-02-01 21:23:18 +0100

    esplorando sys, e studio implementazione indici per esercizio rubrica

---
+++ python/giorno4/exploring_sys.py
@@ -0,0 +1,25 @@
+import sys
+from random import randint
+
+# Controllo degli argomenti
+if len(sys.argv) != 3:
+    print("Uso: indica il range di numeri per cui vuoi provare a indovinare num1, num2")
+    sys.exit(1)
+
+random_number = randint(int(sys.argv[1]), int(sys.argv[2]))
+
+while True:
+    try:
+        number = int(
+            input(f'Please choose a number that falls {sys.argv[1]} and {sys.argv[2]}: '))
+        if int(sys.argv[1]) <= number <= int(sys.argv[2]):
+            if number == random_number:
+                print("You're a genius!")
+                break
+        else:
+            # printo un errore se si digita un numero fuori range
+            print(f"Remember, your number must be between {sys.argv[1]} and {sys.argv[2]}!")
+    except ValueError:
+        # printo un errore se non si digita un numero
+        print("Please enter a number")
+        continue
--- python/giorno4/main.py
+++ python/giorno4/main.py
@@ -57,3 +57,92 @@ class Business:
     # rappresentare obj user in modo leggibile
     def __repr__(self):
         return f"Business(name='{self.name}', phone='{self.phone}')"
+
+
+"""
+Versione da studiare con l'implementazione degli indici di Giulia S.
+Il suo metodo add aggiunge un contatto alla lista di contatti e,
+contemporaneamente, lo indicizza per un accesso più veloce tramite
+vari attributi (nome, cognome, numero di telefono). 
+L’indicizzazione è fatta tramite i dizionari name_index, surname_index e phone_index. 
+
+Nel codice di Giulia ogni volta che un contatto viene aggiunto, 
+viene memorizzato nella lista principale (contacts) e indicizzato 
+nei dizionari name_index, surname_index e phone_index per permettere 
+ricerche più rapide.
+
+Anche Query e find sfruttano gli indici
+
+class Person:
+    def __init__(self, name, surname, phone=None):
+        self.name = name
+        self.surname = surname
+        self.phone = phone
+
+    def __repr__(self):
+        return f"Person(name={self.name}, surname={self.surname}, phone={self.phone})"
+
+
+class Business:
+    def __init__(self, name, phone=None):
+        self.name = name
+        self.phone = phone
+
+    def __repr__(self):
+        return f"Business(name={self.name}, phone={self.phone})"
+
+
+class Directory:
+    def __init__(self):
+        self.contacts = []
+        self.name_index = {}
+        self.surname_index = {}
+        self.phone_index = {}
+
+    def add(self, contact):
+        self.contacts.append(contact)
+        
+        # Se il nome del contatto non è ancora presente nell’indice
+        # name_index, viene creato un nuovo elenco vuoto come valore
+        # associato a quel nome.
+        if contact.name not in self.name_index:
+            self.name_index[contact.name] = []
+        # Aggiunge il contatto all’elenco associato al nome del contatto.
+        self.name_index[contact.name].append(contact)
+
+        if isinstance(contact, Person):
+            if contact.surname not in self.surname_index:
+                self.surname_index[contact.surname] = []
+            self.surname_index[contact.surname].append(contact)
+
+        if contact.phone:
+            if contact.phone not in self.phone_index:
+                self.phone_index[contact.phone] = []
+            self.phone_index[contact.phone].append(contact)
+
+    def __len__(self):
+        return len(self.contacts)
+
+    def query(self, name=None, surname=None):
+        results = []
+        if name:
+            # get cerca il nome nell'indice, se esiste viene aggiunto
+            # alla lista, se non esiste ritorna lista vuota
+            results.extend(self.name_index.get(name, []))
+        if surname:
+            results.extend(self.surname_index.get(surname, []))
+        return results
+
+    def find(self, search_term):
+        results = []
+        # fa cercare in tutti gli indici e poi ricerca parziale nel telefono
+        results.extend(self.name_index.get(search_term, []))
+        results.extend(self.surname_index.get(search_term, []))
+        # questa ricerca per telefono cerca corrispondenze esatte
+        results.extend(self.phone_index.get(search_term, []))
+        #  questa trova corrispondenze parziali
+        for phone, contacts in self.phone_index.items():
+            if search_term in phone:
+                results.extend(contacts)
+        return results
+"""

9a399f3b11a81d7c522859ceec0e39d8769a6661
Veronica Schembri <veronicaschembri@gmail.com>
2025-02-03 12:28:39 +0100

    update guessTheNumber with sys

--- python/giorno4/exploring_sys.py
+++ python/giorno4/exploring_sys.py
@@ -7,15 +7,23 @@ if len(sys.argv) != 3:
     sys.exit(1)
 
 random_number = randint(int(sys.argv[1]), int(sys.argv[2]))
+attempts = 0  # Contatore dei tentativi
+
 
 while True:
     try:
         number = int(
             input(f'Please choose a number that falls {sys.argv[1]} and {sys.argv[2]}: '))
+        attempts += 1  # Incrementa il numero di tentativi
+
         if int(sys.argv[1]) <= number <= int(sys.argv[2]):
             if number == random_number:
-                print("You're a genius!")
+                print(f"You're a genius! You guessed the number in {attempts} attempts.")
                 break
+            elif number < random_number:
+                print("Too low! Try again.")
+            else:
+                print("Too high! Try again.")
         else:
             # printo un errore se si digita un numero fuori range
             print(f"Remember, your number must be between {sys.argv[1]} and {sys.argv[2]}!")
